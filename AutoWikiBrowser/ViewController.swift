68f155a412ab22596d8b999c5d5d6da0089d8a18
dlprice <daniel.price12@imperial.ac.uk>
2017-02-20 10:04:49 +0000

    Initial Commit

---
+++ AutoWikiBrowser/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AutoWikiBrowser
+//
+//  Created by Daniel Price on 20/02/2017.
+//  Copyright © 2017 Daniel Price. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
